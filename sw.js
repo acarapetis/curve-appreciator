3f8594aeef981b584a3c6f2f8f5873237ed123f8
Anthony Carapetis <anthony.carapetis@gmail.com>
2019-07-13 15:33:25 +1000

    Use relative paths in service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 self.addEventListener('install', e => e.waitUntil(
     caches.open('curve-appreciator').then(cache => cache.addAll([
-        '/index.html',
-        '/main.js',
-        '/capture.js',
+        './',
+        './index.html',
+        './main.js',
+        './capture.js',
         'https://cdnjs.cloudflare.com/ajax/libs/acorn/6.2.0/acorn.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.12.2/paper-full.js',
         'https://cdn.jsdelivr.net/gh/spite/ccapture.js@v1.0.9/build/CCapture.all.min.js',
