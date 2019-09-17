f295500689eea3b1a7a0279a20181cd75b0dcbe3
Anthony <anthony.carapetis@gmail.com>
2019-09-17 08:18:38 +0000

    Add PWA Manifest

--- sw.js
+++ sw.js
@@ -4,6 +4,8 @@ self.addEventListener('install', e => e.waitUntil(
         './index.html',
         './main.js',
         './capture.js',
+        './manifest.json',
+        './logo144.png',
         'https://cdnjs.cloudflare.com/ajax/libs/acorn/6.2.0/acorn.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.12.2/paper-full.js',
         'https://cdn.jsdelivr.net/gh/spite/ccapture.js@v1.0.9/build/CCapture.all.min.js',
